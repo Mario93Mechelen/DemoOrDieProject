01b89ea613b7414e065813b5bd24c269c434162d
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-04-23 11:54:27 +0200

    app.js en aparte js files aangepast/toegevoegd (eerste structuur)

--- Demo Or Die/routes/profile.js
+++ Demo Or Die/routes/profile.js
@@ -6,10 +6,10 @@ var name = "Some Name";
 var groups = "Some groups";
 var time = "Some time";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login', {demotitle: demotitle});
-});
+/* GET profile page. */
 
+router.get('/', function(req, res, next) {
+    res.render('profile', {name: name, demotitle: demotitle, groups: groups, time: time })
+});
 
 module.exports = router;
